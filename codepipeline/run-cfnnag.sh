3080448de24d97407d22ad8eb0864a610fe2e000
Kishore Vinjam <vinjak@amazon.com>
2022-02-01 12:06:54 -0500

    Run cfnnag file

---
+++ codepipeline/run-cfnnag.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+shopt -s nullglob
+echo `pwd`
+ls -ltrd
+mkdir -p templates/
+cp {ec2,vpc}/*.{json,yml} templates/
+cp codepipeline/*.json templates/
+
+DATE=$(git log -p -1 --date=relative | grep '^Date:')
+UDATE=$(echo ${DATE} | awk '{print $3}')
+echo "DATE: ${DATE}, UDATE: ${UDATE}"
+
+if [ $UDATE != "seconds" ]
+then
+    for f in $(git log -p -1 | grep '^diff --git' |awk -F" b/" '{print $NF}' | grep '.json'$); do
+        if cfn_nag_scan --input-path "$f" --blacklist-path ./codepipeline/blacklist-cfnnag.yml; then
+            echo "$f PASSED"
+        else
+            echo "$f FAILED"
+            touch FAILED
+        fi
+    done
+
+    if [ -e FAILED ]; then
+      echo cfn-nag FAILED at least once!
+      exit 1
+    else
+      echo cfn-nag PASSED on all files!
+      exit 0
+    fi
+
+else
+    echo "SKIPPING: Last checkin happend ${DATE} ago"
+fi
