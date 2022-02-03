2f6b8df64b8af2588fb6c52ff41c2dccd3934a14
Kishore Vinjam <vinjak@amazon.com>
2022-02-03 07:02:40 -0500

    code fix

--- codepipeline/run-sc-deploy.sh
+++ codepipeline/run-sc-deploy.sh
@@ -11,6 +11,7 @@ then
 else
   echo "$ACODE $ASTR"
   touch FAILED
+fi
 
 ASTR=$((aws cloudformation update-stack --stack-name SC-DEV-EC2-Product --template-url  "https://$DEPLOY_BUCKET.s3.amazonaws.com/templates/devenv/ec2/sc-ec2-linux-ra.json" --parameters '[{"ParameterKey":"RepoRootURL","UsePreviousValue":true}]' --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND) 2>&1)
 ACODE=$?
@@ -20,6 +21,7 @@ then
 else
   echo "$ACODE $ASTR"
   touch FAILED
+fi
 
 ASTR=$((aws cloudformation update-stack --stack-name SC-DEV-MYSQL-Product --template-url  "https://$DEPLOY_BUCKET.s3.amazonaws.com/templates/devenv/ec2/sc-mysql-ra.json" --parameters '[{"ParameterKey":"RepoRootURL","UsePreviousValue":true}]' --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND) 2>&1)
 ACODE=$?
@@ -29,6 +31,7 @@ then
 else
   echo "$ACODE $ASTR"
   touch FAILED
+fi
 
 if [ -e FAILED ]; then
   echo Deploy FAILED at least once!
