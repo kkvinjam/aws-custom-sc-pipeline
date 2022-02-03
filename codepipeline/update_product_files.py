26fa7d196cc830d21b7cfa1761c42bc8e966b363
Kishore Vinjam <vinjak@amazon.com>
2022-02-03 02:30:30 -0500

    scret verify

--- codepipeline/update_product_files.py
+++ codepipeline/update_product_files.py
@@ -48,11 +48,14 @@ def get_secret(secret_name):
     session = boto3.session.Session()
     client = session.client('secretsmanager')
     get_secret_value_response = None
+    print('SECRET NAME: %s', secret_name)
 
     try:
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
+        print('SECRET RESPONSE: %s', get_secret_value_response)
+
     except ClientError as e:
         if e.response['Error']['Code'] == 'DecryptionFailureException':
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
@@ -75,14 +78,14 @@ def get_secret(secret_name):
             # We can't find the resource that you asked for.
             # Deal with the exception here, and/or rethrow at your discretion.
             raise e
-    else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary,
-        # one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
         else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            # Decrypts secret using the associated KMS key.
+            # Depending on whether the secret is a string or binary,
+            # one of these fields will be populated.
+            if 'SecretString' in get_secret_value_response:
+                secret = get_secret_value_response['SecretString']
+            else:
+                decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
 
     return(get_secret_value_response)
 
